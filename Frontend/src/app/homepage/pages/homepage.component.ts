de3042939cd3379223ad38283e1ae8337412fa92
Jose Pablo Ponce <pon19092@uvg.edu.gt>
2023-07-02 20:12:36 -0600

    update homepage
    Update del boton para subir imagenes

--- Frontend/src/app/homepage/pages/homepage.component.ts
+++ Frontend/src/app/homepage/pages/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -11,9 +11,12 @@ export class HomepageComponent implements OnInit {
   assetPath2: string | undefined;
   assetPath3: string | undefined;
   assetPath4: string | undefined;
+  
+  isDragging = false;
 
 
-  constructor() { }
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.assetPath = `${document.baseURI}assets/huevos.png`;
@@ -21,6 +24,42 @@ export class HomepageComponent implements OnInit {
     this.assetPath3 = `${document.baseURI}assets/huevos2.png`;
     this.assetPath4 = `${document.baseURI}assets/camara.png`;
 
+  }
+
+  // Función para controlar el evento 'dragover'
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = true;
+  }
+
+  // Función para controlar el evento 'drop'
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = false;
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      const selectedFile = files[0];
+      // Aquí puedes hacer algo con la imagen seleccionada, como subirla al servidor
+      console.log('Imagen seleccionada:', selectedFile);
+    }
+  }
+
+  // Función para abrir el explorador de archivos al hacer clic en el div
+  abrirExploradorArchivos() {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/*';
+    fileInput.style.display = 'none';
+
+    fileInput.addEventListener('change', (event: any) => {
+      const selectedFile = event.target.files[0];
+      // Aquí puedes hacer algo con la imagen seleccionada, como subirla al servidor
+      console.log('Imagen seleccionada:', selectedFile);
+  
+    });
+
+    document.body.appendChild(fileInput);
+    fileInput.click();
   }
 
 }
